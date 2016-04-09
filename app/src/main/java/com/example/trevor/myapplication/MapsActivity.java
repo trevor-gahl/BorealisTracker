c721f3c49b3fc34f9983e054519368b2f5f14e74
Dustin <dustin.k.spivey@gmail.com>
2016-04-09 01:35:29 -0600

    Route around Jabs :)

--- app/src/main/java/com/example/trevor/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/trevor/myapplication/MapsActivity.java
@@ -1,18 +1,25 @@
 package com.example.trevor.myapplication;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
+import com.google.android.gms.maps.model.*;
+import com.google.android.gms.maps.model.*;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +44,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        ArrayList<LatLng> route = new ArrayList<LatLng>();
+        route.add(new LatLng(45.66984, -111.04866));
+        route.add(new LatLng(45.66981, -111.04881));
+        route.add(new LatLng(45.66975, -111.04894));
+        route.add(new LatLng(45.66966, -111.04903));
+        route.add(new LatLng(45.66955, -111.04906));
+        route.add(new LatLng(45.66935, -111.04906));
+        route.add(new LatLng(45.66851, -111.04906));
+        route.add(new LatLng(45.66849, -111.04922));
+        route.add(new LatLng(45.66843, -111.04932));
+        route.add(new LatLng(45.66835, -111.04936));
+        route.add(new LatLng(45.66827, -111.04934));
+        route.add(new LatLng(45.66817, -111.04924));
+        route.add(new LatLng(45.66815, -111.04906));
+        route.add(new LatLng(45.66815, -111.04889));
+        route.add(new LatLng(45.66822, -111.04875));
+        route.add(new LatLng(45.66833, -111.04872));
+        route.add(new LatLng(45.66845, -111.04866));
+        route.add(new LatLng(45.66832, -111.04857));
+        route.add(new LatLng(45.66822, -111.04856));
+        route.add(new LatLng(45.66815, -111.04847));
+        route.add(new LatLng(45.66812, -111.04829));
+        route.add(new LatLng(45.66814, -111.04811));
+        route.add(new LatLng(45.66819, -111.04795));
+        route.add(new LatLng(45.66832, -111.04789));
+        route.add(new LatLng(45.66843, -111.04795));
+        route.add(new LatLng(45.66853, -111.0481));
+        route.add(new LatLng(45.66854, -111.04826));
+        route.add(new LatLng(45.66936, -111.04829));
+        route.add(new LatLng(45.66954, -111.04829));
+        route.add(new LatLng(45.66964, -111.04834));
+        route.add(new LatLng(45.66974, -111.04842));
+        route.add(new LatLng(45.6698, -111.04854));
+        route.add(new LatLng(45.66984, -111.04866));
+
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .addAll(route)
+                .width(10)
+                .color(Color.BLACK));
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng jabs = new LatLng(45.66905, -111.04871);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jabs, 18));
     }
 }
