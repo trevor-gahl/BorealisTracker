0c8dd51e05440a13bef539e079e2a654f46ec5f0
Trevor <tgahl7@gmail.com>
2016-04-09 01:10:29 -0600

    Transition Buttons

--- app/src/main/java/com/example/trevor/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/trevor/myapplication/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.example.trevor.myapplication;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,5 +44,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    public void transitionMain(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
