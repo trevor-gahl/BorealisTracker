8c55cc572ee2324d645e4fab0ab86ea3382027ec
Trevor <tgahl7@gmail.com>
2016-04-08 22:25:00 -0600

    Main to Maps swipe

--- app/src/main/java/com/example/trevor/myapplication/MainActivity.java
+++ app/src/main/java/com/example/trevor/myapplication/MainActivity.java
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Transition to Map", Snackbar.LENGTH_LONG)
+                        .setAction("MapsActivity", null).show();
             }
         });
     }
