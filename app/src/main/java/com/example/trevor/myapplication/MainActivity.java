b8947283d6d5796c1aff8e4eb9dae6b0904a3785
Trevor <tgahl7@gmail.com>
2016-04-09 14:01:53 -0600

    Design Update 3

--- app/src/main/java/com/example/trevor/myapplication/MainActivity.java
+++ app/src/main/java/com/example/trevor/myapplication/MainActivity.java
@@ -32,12 +32,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+
     public void transitionMaps(View view){
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
@@ -46,18 +41,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ListActivity.class);
         startActivity(intent);
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
